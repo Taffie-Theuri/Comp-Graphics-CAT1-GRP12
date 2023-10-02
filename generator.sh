7261d741a1a0fb609a4449628b1433747fb60883
allanchegek <68268573+allanchegek@users.noreply.github.com>
2023-10-02 13:44:42 +0300

    Create generator.sh

---
+++ generator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Define the data directory and output directory (modify these paths)
+DATA_DIRECTORY="C:\Users\HP\PycharmProjects\massiveDatasetCAT\data"
+OUTPUT_DIRECTORY="C:\Users\HP\PycharmProjects\massiveDatasetCAT\data\q1_py"
+
+# Run the Python script with the specified data and output directories
+python script_name.py "$DATA_DIRECTORY" "$OUTPUT_DIRECTORY"
